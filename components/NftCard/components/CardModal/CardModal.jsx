c31cede999ea04e34df509a5d46f9c6f4cad4e93
Shehryar <14bscssshoukat@seecs.edu.pk>
2021-06-08 22:38:33 +0500

    add CardModal component

---
+++ components/NftCard/components/CardModal/CardModal.jsx
@@ -0,0 +1,125 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import {
+  Avatar,
+  Button,
+  Box,
+  colors,
+  Card,
+  CardContent,
+  CardActionArea,
+  CardHeader,
+  Typography,
+  withStyles,
+} from '@material-ui/core';
+import CancelIcon from '@material-ui/icons/Cancel';
+
+import cardModalStyle from './cardModal.style';
+
+const CardModal = ({ classes, nft }) => {
+  const [showMore, setShowMore] = React.useState(true);
+
+  // eslint-disable-next-line react/prop-types
+  const Owners = ({ avatarSrc, name, creator = true }) => (
+    <Box display="flex" className={classes.owners} alignItems="center">
+      <Typography
+        variant="subtitle1"
+        className={classnames(classes.sfFont, classes.lightFont)}
+      >
+        { creator ? 'Created' : 'Owned' }
+        {' '}
+        by
+      </Typography>
+      <Avatar
+        className={classes.avatar}
+        alt={name}
+        src={avatarSrc}
+      />
+      <Typography
+        variant="subtitle1"
+        className={classnames(classes.sfFont, classes.normalFont)}
+        color="textSecondary"
+      >
+        @
+        {name || 'John Doe'}
+      </Typography>
+    </Box>
+  );
+
+  return (
+    <div className="morph-content">
+      <div>
+        <Card>
+          <CardHeader
+            className={classes.cardHeader}
+            action={(<CancelIcon className="nft-modal-close" style={{ color: colors.red[400] }} />)}
+            title={(
+              <Typography className={classnames(classes.sfFont, classes.normalFont)} variant="subtitle1">{nft.name}</Typography>
+            )}
+          />
+          <CardActionArea>
+            <img
+              src={nft.image_original_url}
+              height="280"
+              width="100%"
+              alt={nft.name}
+            />
+            <CardContent className={classes.cardContent}>
+              <Box>
+                <Typography
+                  className={classnames(classes.sfFont, classes.lightFont, classes.cardContentText)}
+                  variant="body2"
+                  color="textSecondary"
+                  component="p"
+                  noWrap={showMore}
+                >
+                  {nft.description}
+                </Typography>
+                {nft.description.length > 20
+                && (
+                  <Button
+                    className={classes.showMoreButton}
+                    onClick={() => setShowMore((val) => !val)}
+                  >
+                    <Typography
+                      className={classnames(classes.sfFont, classes.lightFont)}
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      { showMore ? 'Show More' : 'Show Less' }
+                    </Typography>
+                  </Button>
+                ) }
+              </Box>
+              <Owners
+                avatarSrc={nft.creator.profile_img_url}
+                name={nft.creator.user.username}
+                creator
+              />
+              <Owners
+                avatarSrc={nft.owner.profile_img_url}
+                name={nft.owner.user.username}
+              />
+              <Button className={classes.bidButton} variant="contained" fullWidth color="primary">
+                <Typography variant="body1" component="span" className={classnames(classes.sfFont, classes.lightFont, classes.bidButtonText)}>
+                  Bid on
+                  {' '}
+                  <span className={classnames(classes.sfFont, classes.normalFont)}>Foundation</span>
+                </Typography>
+              </Button>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+CardModal.propTypes = {
+  classes: PropTypes.object.isRequired,
+  nft: PropTypes.object.isRequired,
+};
+
+export default withStyles(cardModalStyle, { withTheme: true })(CardModal);
---
+++ components/NftCard/components/CardModal/cardModal.style.js
@@ -0,0 +1,48 @@
+import { createStyles } from '@material-ui/core';
+
+const cardModalStyle = ({ spacing }) => createStyles({
+  sfFont: {
+    fontFamily: '"SF Pro", sans-serif',
+  },
+  normalFont: {
+    fontWeight: 600,
+  },
+  lightFont: {
+    fontWeight: 300,
+  },
+  cardHeader: {
+    height: spacing(5),
+    flexDirection: 'row-reverse',
+    textAlign: 'center',
+  },
+  cardContent: {
+    padding: `${spacing(2)}px ${spacing(3)}px`,
+  },
+  cardContentText: {
+    marginBottom: spacing(2),
+    backgroundColor: 'white',
+    lineHeight: '16px',
+  },
+  showMoreButton: {
+    margin: 0,
+    padding: 0,
+  },
+  owners: {
+    marginTop: spacing(1),
+  },
+  avatar: {
+    margin: '0px 10px',
+    border: '2px solid black',
+  },
+  bidButton: {
+    marginTop: spacing(2),
+    textTransform: 'capitalize',
+    backgroundColor: '#464646',
+    padding: spacing(1.8),
+  },
+  bidButtonText: {
+    letterSpacing: '1px',
+  },
+});
+
+export default cardModalStyle;
